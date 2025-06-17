64d2a996b953a1717f749f8b08261c577be3111f
Narjisha K <144308345+NarjishaK@users.noreply.github.com>
2025-06-17 18:08:52 +0530

    update

--- app.js
+++ app.js
@@ -34,13 +34,11 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:3002",
       "http://localhost:3001",
       "http://localhost:3000",
       "https://dz.sstappstore.in",
       "https://deelzon.com",
+      "https://api.sstappstore.in"
     ],
     method: ["PUT", "DELETE", "PUSH", "GET", "POST", "PATCH"],
     credential: true,
